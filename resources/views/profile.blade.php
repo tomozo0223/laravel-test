85d191479ea752f67858cd40076ccfc8a4b9457c
tomoki <tmdesign.0223@gmail.com>
2025-12-23 21:29:34 +0900

    layouts追加 継承

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,29 +1,20 @@
-<!DOCTYPE html>
-<html lang="ja">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
-</head>
+@section('title', 'プロフィールページ')
 
-<body>
+@section('content')
     <h1>Profile</h1>
     <h2>ようこそ{{ $name }}さん!</h2>
-
     @if ($is_admin)
         <p>あなたは管理者です</p>
     @else
         <p>あなたは一般Peopleです</p>
     @endif
-
     <ul>
         @foreach ($skills as $skill)
             <li>{{ $skill }}</li>
         @endforeach
     </ul>
-
     {{-- ログイン状態なら表示 --}}
     @auth
         <p>ログイン中です</p>
@@ -33,6 +24,4 @@
         <button>ログイン</button>
         <p>非ログイン中です</p>
     @endguest
-</body>
-
-</html>
+@endsection
